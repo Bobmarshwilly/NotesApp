69f3018c2ba3f5d0452f6152bc163369db9a5edc
Bobmarshwilly <danilaloginov5@gmail.com>
2025-05-07 00:08:17 +0300

    Форматирование с помощью ruff

--- alembic/versions/b2f23a50f099_init.py
+++ alembic/versions/b2f23a50f099_init.py
@@ -1,10 +1,11 @@
 """init
 
 Revision ID: b2f23a50f099
-Revises: 
+Revises:
 Create Date: 2025-04-17 01:49:33.138419
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b2f23a50f099'
+revision: str = "b2f23a50f099"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -20,13 +21,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.create_table('notes',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('content', sa.String(length=255), nullable=False),
-    sa.PrimaryKeyConstraint('id')
+    op.create_table(
+        "notes",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("content", sa.String(length=255), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
     )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.drop_table('notes')
+    op.drop_table("notes")

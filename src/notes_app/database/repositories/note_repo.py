7663727520ef1ed3d2823de9178b5f70ebf2737e
Bobmarshwilly <danilaloginov5@gmail.com>
2025-05-09 18:20:25 +0300

    Добавление mypy в CI

--- src/notes_app/database/repositories/note_repo.py
+++ src/notes_app/database/repositories/note_repo.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from notes_app.database.models import Note
@@ -7,10 +9,10 @@ class NoteRepo:
     def __init__(self, session: Session):
         self._session = session
 
-    def add_note(self, note: Note):
+    def add_note(self, note: Note) -> None:
         self._session.add(note)
 
-    def get_notes(self) -> list[Note]:
+    def get_notes(self) -> Sequence[str]:
         stmt = select(Note.content)
         notes = self._session.scalars(stmt).all()
         return notes
